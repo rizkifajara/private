305d43358bc73ac3414906e88b62f0ab14d60272
rizkifajara <rizkifajar456@gmail.com>
2021-08-20 22:52:12 +0700

    edited main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    render_template("layout.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
