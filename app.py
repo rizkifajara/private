c0128fd54d04dc20454a6c51b864d1d17e194450
rizkifajara <rizkifajar456@gmail.com>
2021-08-21 23:33:24 +0700

    added api form

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template
+from flask_pymongo import pymongo
+import pandas as pd
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage
+from pymongo import MongoClient
+import json
 import os
 
 app = Flask(__name__)
@@ -6,6 +13,60 @@ app = Flask(__name__)
 @app.route("/")
 def helloWorld():
     return render_template("layout.html")
+
+@app.route("/form")
+def upload_form():
+    
+    client = pymongo.MongoClient("mongodb+srv://johndoe:johndoe@cluster0.jyb2o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    db = client.test
+    
+    database_name='hackuna_matata123'
+    student_db=client[database_name]
+
+    collection_name='user'
+    collection=student_db[collection_name]
+    # f = request.files.get('file')
+    # mf = dataiku.Folder('O2B4wCQL') # name of the folder in the flow
+    # target_path = '/%s' % f.filename
+    # mf.upload_stream(target_path, f)
+
+    well = request.form.get('well')
+    lat = request.form.get('lat')
+    lon = request.form.get('lon')
+
+
+    df = pd.read_csv(mf.get_download_stream(f.filename))
+    
+    source = {
+        # "filename":f.filename,
+        #   "coordinate ": {"BAND":None, "EASTING":None,
+        #                   "LATITUDE": lat, "LONGITUDE": lon,
+        #                   "NORTHING": None, "WELL":well,
+        #                   "ZONE": None},
+        #   "data":[]
+        "wellName": well,
+        "latitude": lat,
+        "longitude": lon
+    }
+    
+          
+    
+    # for i in range(len(df)):
+    #     dict_df = {}
+        
+    #     for col in df.columns:
+    #         dict_df['WELL']=well
+    #         dict_df[col]=df[col].iloc[i]
+            
+    #     source['data'].append(dict_df)
+    
+    print(source)
+    
+    collection.insert_one(source)
+    
+    return json.dumps({1})
+
+
 
 
 if __name__ == "__main__":
