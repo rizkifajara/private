587ca2fbb2666fed49935b0f80d83a9bc7f47ce3
rizkifajara <rizkifajar456@gmail.com>
2021-08-23 17:55:21 +0700

    html form added

--- app.py
+++ app.py
@@ -15,9 +15,11 @@ app = Flask(__name__)
 def helloWorld():
     return render_template("layout.html")
 
-@app.route("/")
-
 @app.route("/form")
+def viewForm():
+    return render_template("form.html")
+
+@app.route("/post_form")
 def upload_form():
     
     client = pymongo.MongoClient("mongodb+srv://johndoe:johndoe@cluster0.jyb2o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
