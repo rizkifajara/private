3609647a4329274e9cf4127c95fe1c99bb917280
rizkifajara <rizkifajar456@gmail.com>
2021-08-28 13:18:16 +0700

    fixed upload to dataiku + redirect

---
+++ getData.py
@@ -0,0 +1,10 @@
+import dataiku
+
+def get_data_from_dataiku(name):
+  host = "https://ai-delfi-ing-hackathon.datascience.delfi.slb.com/"
+  apiKey = "KQzhMgvVpsIF3ojSjeg12L9rdp9zdFAP"
+  projectKey = "DEMO_HACKUNAMATATA"
+
+  dataiku.set_remote_dss(host, apiKey, no_check_certificate=True)
+  result = dataiku.Dataset(name, project_key=projectKey).get_dataframe()
+  return result
